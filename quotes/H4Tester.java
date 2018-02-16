a5ac9c7ad5cc1b1285e610c3fa40028ea07c3f54
Molly <mthomson1196@gmail.com>
2018-02-16 12:55:20 -0500

    search 1 Test

--- quotes/H4Tester.java
+++ quotes/H4Tester.java
@@ -22,27 +22,67 @@ public class H4Tester {
 	
 	@Test
 	public void search1() {
-		String searchString = ("Molly");
+		String searchString = ("Seho");
 		quoteList.search(searchString, 0);
-		// Expecting 3 search results for author Molly 
-		for (int i = 0; i < 3; i++) {
-			assertEquals(
-					quoteList.getQuote(quoteList.getSize() - 1 - i).getAuthor().contains(searchString) ||
-					quoteList.getQuote(quoteList.getSize() - 1 - i).getAuthor().contains(searchString.toLowerCase()),
-					true);
-		}
+		// Expecting 4 search results for author Molly 
+//		for (int i = 0; i < 3; i++) {
+//			assertEquals(
+//					quoteList.getQuote(quoteList.getSize() - 1 - i).getAuthor().contains(searchString) ||
+//					quoteList.getQuote(quoteList.getSize() - 1 - i).getAuthor().contains(searchString.toLowerCase()),
+//					true);
+//		}
 		
-		// Will fail if check beyond 3 (since there was only 3 quotes with Molly as author)
+		// Will fail if check beyond 4 (since there was only 3 quotes with Molly as author)
 		assertEquals(
-				quoteList.getQuote(quoteList.getSize() - 1 - 4).getAuthor().contains(searchString) ||
-				quoteList.getQuote(quoteList.getSize() - 1 - 4).getAuthor().contains(searchString.toLowerCase()),
-				false);
+				quoteList.getQuote(quoteList.getSize() - 1 - 1).getAuthor().contains(searchString) ||
+				quoteList.getQuote(quoteList.getSize() - 1 - 1).getAuthor().contains(searchString.toLowerCase()),
+				true);
+		System.out.println(quoteList.getQuote(quoteList.getSize() - 1 - 4));
+		System.out.println(quoteList.getQuote(quoteList.getSize() - 1 - 3));
+		System.out.println(quoteList.getQuote(quoteList.getSize() - 1 - 2));
+		System.out.println(quoteList.getQuote(quoteList.getSize() - 1 - 1));
+		System.out.println(quoteList.getQuote(quoteList.getSize() - 1 - 0));
 	}
 	
-	@Test
-	public void search2() {
-		assertEquals(quoteList.search("molly", 1).getSize() == 0, true);
-		//assertEquals(quoteList.search(""))
-	}
+//	@Test
+//	public void search2() {
+//		String searchString = ("Ubuntu");
+//		quoteList.search(searchString, 1);
+//		// Expecting one result
+//		assertEquals(
+//				quoteList.getQuote(quoteList.getSize() - 1).getQuoteText().contains(searchString) ||
+//				quoteList.getQuote(quoteList.getSize() - 1).getQuoteText().contains(searchString.toLowerCase()),
+//				true);
+//		
+//		// Will fail if we check for more than one result
+//		assertEquals(
+//				quoteList.getQuote(quoteList.getSize() - 2).getQuoteText().contains(searchString) ||
+//				quoteList.getQuote(quoteList.getSize() - 2).getQuoteText().contains(searchString.toLowerCase()),
+//				false);
+//	}
+	
+//	public void search3() {
+//		String searchString = ("Molly");
+//		quoteList.search(searchString, 2);
+//		// Expecting four results
+//		for(int i = 0; i < 4; i++) {
+//		assertEquals(
+//				quoteList.getQuote(quoteList.getSize() - 1 - i).getQuoteText().contains(searchString) ||
+//				quoteList.getQuote(quoteList.getSize() - 1).getQuoteText().contains(searchString.toLowerCase()) ||
+//				quoteList.getQuote(quoteList.getSize() - 1 - i).getAuthor().contains(searchString) ||
+//				quoteList.getQuote(quoteList.getSize() - 1 - i).getAuthor().contains(searchString.toLowerCase()),
+//				true);
+//		}
+//		
+//		// Will fail if we check for more than four results
+//		assertEquals(
+//				quoteList.getQuote(quoteList.getSize() - 6).getQuoteText().contains(searchString) ||
+//				quoteList.getQuote(quoteList.getSize() - 6).getQuoteText().contains(searchString.toLowerCase()) ||
+//				quoteList.getQuote(quoteList.getSize() - 6).getAuthor().contains(searchString) ||
+//				quoteList.getQuote(quoteList.getSize() - 6).getAuthor().contains(searchString.toLowerCase()),
+//				false);
+//	}
+	
+	
 
 }
