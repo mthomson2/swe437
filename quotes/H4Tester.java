c5086a54b65bd4722deb9143dab22230a0df9639
Molly <mthomson1196@gmail.com>
2018-02-16 12:11:52 -0500

    search 1 Test

--- quotes/H4Tester.java
+++ quotes/H4Tester.java
@@ -22,7 +22,27 @@ public class H4Tester {
 	
 	@Test
 	public void search1() {
-		assertEquals(quoteList.search("molly", 0).getSize() != 0, true);
+		String searchString = ("Molly");
+		quoteList.search(searchString, 0);
+		// Expecting 3 search results for author Molly 
+		for (int i = 0; i < 3; i++) {
+			assertEquals(
+					quoteList.getQuote(quoteList.getSize() - 1 - i).getAuthor().contains(searchString) ||
+					quoteList.getQuote(quoteList.getSize() - 1 - i).getAuthor().contains(searchString.toLowerCase()),
+					true);
+		}
+		
+		// Will fail if check beyond 3 (since there was only 3 quotes with Molly as author)
+		assertEquals(
+				quoteList.getQuote(quoteList.getSize() - 1 - 4).getAuthor().contains(searchString) ||
+				quoteList.getQuote(quoteList.getSize() - 1 - 4).getAuthor().contains(searchString.toLowerCase()),
+				false);
+	}
+	
+	@Test
+	public void search2() {
+		assertEquals(quoteList.search("molly", 1).getSize() == 0, true);
+		//assertEquals(quoteList.search(""))
 	}
 
 }
